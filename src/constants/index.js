3fc755f17394d8893a34976af8baae39c61c2008
Bakare Damilola <bdamilola00@gmail.com>
2024-02-15 12:05:23 +0100

    add: new project object

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,4 +1,4 @@
-import {  linkedin, twitter, github, project1, project2, project3 } from "../assets/images";
+import {  linkedin, twitter, github, project1, project2, project3, projectify } from "../assets/images";
 
 export  const navLinks = [
     { page: "Home", href: "Home" },
@@ -15,6 +15,7 @@ export  const navLinks = [
 
 
 export const projects = [
+  { title: "Projectify",image: projectify, stack: "NextJS", description : "AI tools for project Engineers", link: "https://projectify-nine.vercel.app/"},
   { title: "Naomi", image: project2, stack: "React, FastAPI", description : "AI Voice Assitance", link: "https://github.com/Earnstein/react-projects/tree/main/eAI"},
   { title: "eShop", image: project1, stack: "MERN", description : "E-commerce store", link: "https://github.com/Earnstein/react-projects/tree/version-3/eShop"},
   { title: "Earneats",image: project3,stack: "Django", description : "Online Food Marketplace", link: "https://github.com/Earnstein/python-fiesta/tree/master/earneats"},
